13fce6044d8db753b777cbf8f7148f472ab2b3dc
Martin <martin.ng2001@gmail.com>
2024-07-17 17:17:11 +0800

    Add transcript

--- react-video-call/src/App.tsx
+++ react-video-call/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import ChatBox from "./components/ChatBox";
 import VideoCall from "./components/VideoCall";
 import { socket } from "./socket";
+import Transcript from "./components/Transcript";
 
 const App = () => {
   useEffect(() => {
@@ -17,8 +18,9 @@ const App = () => {
   return (
     <div className="h-screen bg-black flex flex-col justify-center items-center">
       <div className="grid grid-cols-4 w-full h-full">
-        <div className="col-span-3 bg-blue-100 justify-center items-center">
+        <div className="relative col-span-3 bg-blue-100 justify-center items-center">
           <VideoCall />
+          <Transcript />
         </div>
         <div className="col-span-1 bg-white p-2 flex flex-col items-center shadow-lg h-full">
           <h1 className="text-3xl font-bold mb-2 border-b-2">Bot Insights</h1>
--- react-video-call/src/components/Transcript.tsx
+++ react-video-call/src/components/Transcript.tsx
@@ -1,15 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { socket } from "../socket";
 
 const Transcript = () => {
+  const [transcript, setTranscript] = useState<string>(
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur`
+  );
+
   useEffect(() => {
-    //TODO: Transcript
     socket.on("transcript", (data) => {
       console.log(data);
+      setTranscript(data.transcript);
     });
   }, []);
 
-  return <></>;
+  return (
+    <div className="absolute bottom-8 left-4 right-4 max-w-4xl mx-auto p-4 bg-white/60 backdrop-blur-lg rounded-lg shadow-lg">
+      <p className="text-gray-800">{transcript}</p>
+    </div>
+  );
 };
 
 export default Transcript;
